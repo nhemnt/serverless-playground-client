e2e668823de153bc5420ff6679609670518c7936
hemant negi <nhemnt@gmail.com>
2020-09-27 13:07:01 +0530

    add loader on sign in

--- src/containers/Login.js
+++ src/containers/Login.js
@@ -6,7 +6,8 @@ import { useHistory } from "react-router-dom";
 import { useAppContext } from "../libs/contextLib";
 
 import "./Login.css";
-
+import LoaderButton from "../components/LoaderButton";
+import { onError } from "../libs/errorLib";
 
 export default function Login() {
   const history = useHistory();
@@ -15,19 +16,22 @@ export default function Login() {
 
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
-  
+  const [isLoading, setIsLoading] = useState(false);
+
   function validateForm() {
     return email.length > 0 && password.length > 0;
-  } 
+  }
 
   async function handleSubmit(event) {
     event.preventDefault();
+    setIsLoading(true);
     try {
       await Auth.signIn(email, password);
       userHasAuthenticated(true);
       history.push("/");
     } catch (e) {
-      alert(e.message);
+      onError(e);
+      setIsLoading(false);
     }
   }
 
@@ -51,10 +55,15 @@ export default function Login() {
             type="password"
           />
         </FormGroup>
-        <Button block bsSize="large" disabled=
-          {!validateForm()} type="submit">
+        <LoaderButton
+          block
+          type="submit"
+          bsSize="large"
+          isLoading={isLoading}
+          disabled={!validateForm()}
+        >
           Login
-</Button>
+        </LoaderButton>
       </form>
     </div>);
 }
