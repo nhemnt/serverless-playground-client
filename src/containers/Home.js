9a7f5d16063ec5f41cd0bf7a5dd5d261bd1d530b
hemant negi <nhemnt@gmail.com>
2020-09-27 14:18:59 +0530

    refactor home page for list of notes

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -1,13 +1,38 @@
-import React from 'react'
+import React, { useState, useEffect } from "react";
+import { ListGroup, ListGroupItem } from "react-bootstrap";
+import { useAppContext } from "../libs/contextLib";
+import { onError } from "../libs/errorLib";
 import "./Home.css";
 const Home = () => {
-  return (
-    <div className="Home">
-      <div className="lander"><h1>Scratch</h1>
+  const [notes, setNotes] = useState([]);
+  const { isAuthenticated } = useAppContext();
+  const [isLoading, setIsLoading] = useState(true);
+
+  function renderNotesList(notes) {
+    return null;
+  }
+  function renderLander() {
+    return (
+      <div className="lander">
+        <h1>Notes</h1>
         <p>A simple note taking app</p>
       </div>
-    </div>
-  )
-}
+    );
+  }
 
-export default Home
+  function renderNotes() {
+    return (
+      <div className="notes">
+        <h1>Your Notes</h1>
+        <ListGroup>{!isLoading && renderNotesList(notes)}</ListGroup>
+      </div>
+    );
+  }
+  return (
+    <div className="Home">
+      {isAuthenticated ? renderNotes() : renderLander()}
+    </div>
+  );
+};
+
+export default Home;
