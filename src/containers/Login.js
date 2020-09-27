a5f20547755948a840b142ce01879f12ceb58e62
hemant negi <nhemnt@gmail.com>
2020-09-27 13:32:33 +0530

    add signup form

--- src/containers/Login.js
+++ src/containers/Login.js
@@ -5,28 +5,32 @@ import { useHistory } from "react-router-dom";
 
 import { useAppContext } from "../libs/contextLib";
 
-import "./Login.css";
+import { useFormFields } from "../libs/hooksLib";
 import LoaderButton from "../components/LoaderButton";
 import { onError } from "../libs/errorLib";
+
+import "./Login.css";
 
 export default function Login() {
   const history = useHistory();
 
   const { userHasAuthenticated } = useAppContext();
 
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
+  const [fields, handleFieldChange] = useFormFields({
+    email: "",
+    password: ""
+  });
   const [isLoading, setIsLoading] = useState(false);
 
   function validateForm() {
-    return email.length > 0 && password.length > 0;
+    return fields.email.length > 0 && fields.password.length > 0;
   }
 
   async function handleSubmit(event) {
     event.preventDefault();
     setIsLoading(true);
     try {
-      await Auth.signIn(email, password);
+      await Auth.signIn(fields.email, fields.password);
       userHasAuthenticated(true);
       history.push("/");
     } catch (e) {
@@ -43,15 +47,15 @@ export default function Login() {
           <FormControl
             autoFocus
             type="email"
-            value={email}
-            onChange={e => setEmail(e.target.value)}
+            value={fields.email}
+            onChange={handleFieldChange}
           />
         </FormGroup>
         <FormGroup controlId="password" bsSize="large">
           <FormLabel>Password</FormLabel>
           <FormControl
-            value={password}
-            onChange={e => setPassword(e.target.value)}
+            value={fields.password}
+            onChange={handleFieldChange}
             type="password"
           />
         </FormGroup>
